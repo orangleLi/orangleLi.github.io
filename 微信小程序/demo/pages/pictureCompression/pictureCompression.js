5ef35f3c6aa340de3de24ca8bad10f289fef5b27
lilian <18291003389@163.com>
2019-08-22 13:52:06 +0800

    图片压缩

--- 微信小程序/demo/pages/pictureCompression/pictureCompression.js
+++ 微信小程序/demo/pages/pictureCompression/pictureCompression.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imageList: []
+    imagePath: '',
+    quality: 0.2
   },
 
   /**
@@ -14,7 +15,6 @@ Page({
   onLoad: function (options) {
 
   },
-
 
   /**
   * 选项添加图片事件
@@ -25,32 +25,18 @@ Page({
       sizeType: ['compressed'],  //可选择原图或压缩后的图片
       sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
       success: result => {
-        console.log('图片原大小：' + result.tempFiles[0].size)
         wx.getImageInfo({
           src: result.tempFilePaths[0],
           success: function (res) {
-            that.getCanvasSize(res.width, res.height, function (canvasWidth, canvasHeight) {
-              that.setData({
-                cWidth: canvasWidth,
-                cHeight: canvasHeight
-              })
-              that.getCanvasImg(result.tempFilePaths, canvasWidth, canvasHeight, function (res) {
-                const images = that.data.imageList.concat(res.tempFilePath);
-                if (images && images.length > 3) {
-                  wx.showToast({
-                    title: '最多3张图片哦',
-                    icon: 'none',
-                    duration: 2000
-                  })
-                }
-                // 限制最多只能留下3张照片
-                let imageList = images.length <= 3 ? images : images.slice(0, 3);
-                that.setData({
-                  imageList: imageList
-                });
-              });
+            that.setData({
+              cWidth: res.width,
+              cHeight: res.height
             })
-
+            that.getCanvasImg(result.tempFilePaths, res.width, res.height, that.data.quality, function (res) {
+              that.setData({
+                imagePath: res.tempFilePath
+              });
+            });
           }
         })
 
@@ -59,41 +45,19 @@ Page({
 
   },
 
-  save(e) {
-    let that = this;
-    let index = e.currentTarget.dataset.index;
-    wx.saveImageToPhotosAlbum({
-      filePath: that.data.imageList[index],
-      success: function (res) {
-        console.log('图片已保存');
-      },
-      fail: function (res) {
-        console.log('保存失败');
-      }
-    })
-  },
-
-  getCanvasSize (width, height, callback){
-    var that = this;
-    var ratio = 2.5;
-    var canvasWidth = width //图片原始长宽
-    var canvasHeight = height
-    while (canvasWidth > 800 || canvasHeight > 800) {// 保证宽高在400以内
-      canvasWidth = Math.trunc(width / ratio)
-      canvasHeight = Math.trunc(height / ratio)
-      ratio++;
-    }
-    callback && callback(canvasWidth, canvasHeight);
-  },
-
-  getCanvasImg(tempFilePaths, canvasWidth, canvasHeight, callback) {
-    var that = this;
+  /**
+   * 质量压缩
+   */
+  getCanvasImg(tempFilePaths, canvasWidth, canvasHeight, quality, callback) {
+    var that = this; 
     const ctx = wx.createCanvasContext('attendCanvasId');
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.drawImage(tempFilePaths[0], 0, 0, canvasWidth, canvasHeight);
     ctx.draw(false, function () {
       wx.canvasToTempFilePath({
         canvasId: 'attendCanvasId',
+        fileType: 'jpg',
+        quality: quality,
         success: function success(res) {
           callback && callback(res)
         }, fail: function (e) {
@@ -107,51 +71,18 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 图片保存到相册
    */
-  onReady: function () {
-
+  save(e) {
+    let that = this;
+    wx.saveImageToPhotosAlbum({
+      filePath: that.data.imagePath,
+      success: function (res) {
+        console.log('图片已保存');
+      },
+      fail: function (res) {
+        console.log('保存失败');
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
